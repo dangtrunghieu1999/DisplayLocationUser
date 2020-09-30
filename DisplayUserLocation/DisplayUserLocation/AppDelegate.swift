a31be6202563050a8f93f16388d2972aa6aa8a6f
Dang Trung Hieu <hieudt230899@gmail.com>
2020-09-30 11:49:31 +0700

    complete get location mapkit

--- DisplayUserLocation/DisplayUserLocation/AppDelegate.swift
+++ DisplayUserLocation/DisplayUserLocation/AppDelegate.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import SnapKit
 
 @UIApplicationMain
+
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
         return true
     }
-
 
 }
 
--- DisplayUserLocation/DisplayUserLocation/ViewController.swift
+++ DisplayUserLocation/DisplayUserLocation/ViewController.swift
@@ -7,14 +7,80 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Variables
+    
+    var locationManager: CLLocationManager!
+    var mapView: MKMapView!
+    
+    // MARK: - UI Elements
+    
+    // MARK: - View LifeCycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureLocationManager()
+        configureMapView()
+        enableLocationServices()
+        centerMapOnUserLocation()
     }
+    
+    // MARK:- Helper Method
+    
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else {return }
+        let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 2000, longitudinalMeters: 2000)
+        mapView.setRegion(region, animated: true)
+        
+    }
+    
+    func configureLocationManager() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+    }
+    
+    func configureMapView() {
+        mapView = MKMapView()
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        view.addSubview(mapView)
+        mapView.frame = view.frame
+    }
+    
+    
+    // MARK: - UI Actions
+    
+    // MARK: - Public Method
+    
+    // MARK: - Layouts
+    
+}
 
-
+extension ViewController: CLLocationManagerDelegate {
+    
+    func enableLocationServices() {
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            print("Location auth status is not determined")
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("Location auth status is not restricted")
+        case .denied:
+            print("Location auth status is not denied")
+        case .authorizedAlways:
+            print("Location auth status is not authorizedAlways")
+        case .authorizedWhenInUse:
+            print("Location auth status is not authorizedWhenInUse")
+        default:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+    }
 }
 
