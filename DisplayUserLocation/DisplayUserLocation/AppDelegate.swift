dbab8cddde0da56b9b1fe8020425912a087f3696
Dang Trung Hieu <hieudt230899@gmail.com>
2020-09-30 10:30:21 +0700

    * Create source * Add framework SnapKit, SDWebImage

---
+++ DisplayUserLocation/DisplayUserLocation/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  DisplayUserLocation
+//
+//  Created by Dang Trung Hieu on 9/30/20.
+//  Copyright © 2020 Dang Trung Hieu. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ DisplayUserLocation/DisplayUserLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DisplayUserLocation
+//
+//  Created by Dang Trung Hieu on 9/30/20.
+//  Copyright © 2020 Dang Trung Hieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
